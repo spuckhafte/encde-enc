09542960157cc196b57d00a5fda0628cb50de01e
Rakshit <70335252+spuckhafte@users.noreply.github.com>
2022-04-17 17:45:26 +0530

    ::ignore::

--- EncryptionMachine.js
+++ EncryptionMachine.js
@@ -1,4 +1,4 @@
-export default class EncryptionMachine {
+class EncryptionMachine {
     constructor() {
         this.config = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28]
     }
@@ -49,7 +49,9 @@ export default class EncryptionMachine {
             }
             console.log('\n\x1b[33m' + ((f+1 - same)/f * 100).toString() + '%\x1b[0m \x1b[32mof the encryptions are unique')
             console.log('\x1b[33m' + ((f+1 - cor)/f * 100).toString() + '%\x1b[0m \x1b[32mof the encryptions are correct\n')
-            console.log('Total Iterations: \x1b[33m' + f + '\x1b[0m \x1b[32m\nWord: \x1b[33m' + word + '\x1b[0m\n')
+            console.log('Total Iterations: \x1b[33m' + f + '\x1b[0m \x1b[32m\nString: \x1b[33m' + word + '\x1b[0m\n')
         } else console.log('\x1b[31mInvalid arguments\x1b[0m')
     }
 }
+
+module.exports = EncryptionMachine
